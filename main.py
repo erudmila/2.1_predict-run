80ff9e197fc172bdb1978710b1f6f8ff5dddafbf
erudmila <144694341+erudmila@users.noreply.github.com>
2024-03-19 13:11:24 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+#Predict and Run Example 1
+ # it will output the name "Andy"
+
+firstName = "Andy"
+
+print(firstName)
+
+#Predict and Run Example 2
+ # will display his full name
+
+lastName = "Colley"
+
+fullName = firstName + " " + lastName
+
+print(fullName)
+
+#Predict and Run Example 3
+ # i predict that this wil display his full name starting with hello Andy. your full name is "Andy Colley"
+
+print("Hello " + firstName + ". Your full name is " + fullName + ".")
+
